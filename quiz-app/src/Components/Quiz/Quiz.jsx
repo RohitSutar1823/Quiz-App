a0c5e576bed95620d7b274870533b5fc40972f55
Rohit Sutar <rohit1823sutar@gmail.com>
2026-08-25 16:53:55 +0530

    feat: add quiz question UI with options, next button, and question counter

---
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>Which device is requied for Internet Connection</h2>
+      <ul>
+        <li>Modem</li>
+        <li>Router</li>
+        <li>Lan cable</li>
+        <li>Pen drive</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 questions</div>
+    </div>
+  )
+}
+
+export default Quiz
